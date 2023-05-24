36af62ebdd0873b2cf87dfd843fe8afa7cfb1203
athulpm <97athulpm@gmail.com>
2023-05-24 09:13:49 +0530

    Added about page

---
+++ src/components/pages/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NavBar />
+    <h1>This is the about page</h1>
+  </div>
+</template>
+
+<script>
+import NavBar from "../UI/NavBar.vue";
+
+export default {
+  name: "AboutPage",
+  components: {
+    NavBar,
+  },
+};
+</script>
--- src/router/router.js
+++ src/router/router.js
@@ -4,9 +4,11 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 import Home from '../components/pages/Home.vue';
+import About from '../components/pages/About.vue'
 
 const routes = [
-    {path: '/', component: Home}
+    {path: '/', component: Home},
+    {path: '/about', component: About}
 ];
 
 const router = new VueRouter({
