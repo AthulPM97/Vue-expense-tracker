86fadb394e013458772a3e8a50b1c1dc795dfc16
athulpm <97athulpm@gmail.com>
2023-05-30 18:32:28 +0530

    Added mutation and action

--- src/components/pages/Expenses.vue
+++ src/components/pages/Expenses.vue
@@ -1,18 +1,32 @@
 <template>
-    <div>
-        {{ expenses }}
-    </div>
+  <div>
+    user: {{ userName }}
+    {{ expenses }}
+    <!-- <input type="text" placeholder="enter new name"/> -->
+    <button @click="changeName">Change name</button>
+  </div>
 </template>
 
 <script>
-    // import { mapState } from 'vuex';
-    // console.log(this)
-    export default {
-        name: 'ExpenseView',
-        computed: {
-            expenses() {
-                return this.$store.state.expense.expenses
-            }
-        }
-    }
+import { mapState } from "vuex";
+// console.log(this)
+export default {
+  name: "ExpenseView",
+  computed: {
+    // expenses() {
+    //     return this.$store.state.expense.expenses
+    // }
+    ...mapState({
+      expenses: (state) => state.expense.expenses,
+      userName: (state) => state.expense.currentUser,
+    }),
+  },
+  methods: {
+    changeName() {
+       this.$store.dispatch("expense/changeName", {
+        newName: "Tom",
+      });
+    },
+  },
+};
 </script>
--- src/store/expense.js
+++ src/store/expense.js
@@ -1,13 +1,19 @@
 export default {
-    namespaced: true,
+  namespaced: true,
 
-    state: {
-        expenses: [{id: 1, amount: 100, description: 'Milk shake'}],
-    }, 
-    mutations: {
-
-    }, 
-    actions: {
-
-    }
-}
+  state: {
+    expenses: [{ id: 1, amount: 100, description: "Milk shake" }],
+    currentUser: "John",
+  },
+  mutations: {
+    set_currentUser(state, data) {
+      state.currentUser = data;
+    },
+  },
+  actions: {
+    changeName({ commit }, data) {
+        console.log(data)
+      commit("set_currentUser", data.newName);
+    },
+  },
+};
